3c10f5ab4c83119c0014d05b331462a75f4ee72a
hafizhahfarahh <hafizhahfarahfadhila@gmail.com>
2025-05-11 22:49:15 +0700

    PPL4-51 PPL4-52 PPL4-53 Membuat Migrasi
    Membuat migrasi testimoni dan membuat table testimoni di database

---
+++ project_OrangBaik/database/migrations/2025_05_11_145014_create_testimoni_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('testimoni', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('lokasi');
+            $table->string('jenis_bencana');
+            $table->text('isicerita');
+            $table->string('foto')->nullable();
+            $table->enum('status', ['pending', 'verified', 'rejected'])->default('pending'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('testimoni');
+    }
+};
